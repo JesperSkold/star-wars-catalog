f7e1d7982e43b33fd5de41d35dec1c644163af51
Jesper <jesper.olsson@iths.se>
2022-02-11 13:22:02 +0100

    render function for chars, add prev/next btn

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,48 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    characters: [],
+    currPage: 1,
+    currPlanet: null,
+    maxPages: 0
   },
   mutations: {
+    saveChars(state, chars) {
+      state.characters = chars
+    },
+    nextPage(state) {
+      if (state.currPage === state.maxPages) {
+        return
+      }
+      state.currPage++
+    },
+    prevPage(state) {
+      if (state.currPage === 1) {
+        return
+      }
+      state.currPage--
+    },
+    setMaxPages(state, maxPages){
+      state.maxPages = maxPages
+    }
   },
   actions: {
+    async fetchChars(context) {
+      const request = await fetch(`https://swapi.dev/api/people?page=${context.state.currPage}`)
+      const data = await request.json()
+      if (context.state.maxPages === 0) {
+        context.commit('setMaxPages', Math.ceil(data.count / 10))
+      }
+      context.commit('saveChars', data.results)
+    },
+    async fetchNextPage(context) {
+      context.commit('nextPage')
+      await context.dispatch('fetchChars')
+    },
+    async fetchPrevPage(context) {
+      context.commit('prevPage')
+      await context.dispatch('fetchChars')
+    },
   },
   modules: {
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,67 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="wrapper">
+		<header>
+			<h1>Star Wars Catalog</h1>
+			<p class="starwarsQuote"></p>
+		</header>
+		<main>
+			<section>
+				<h2 class="characterBackground">Characters</h2>
+				<ul class="characters">
+					<li v-for="char of chars" :key="char.name">{{ char.name }}</li>
+				</ul>
+				<div class="paginator">
+					<span class="prevBtn" @click="prevPage">&#9665;</span>
+					<span class="numberList">{{ currPage }} / {{ maxPage }}</span>
+					<span class="nextBtn" @click="nextPage">&#9655;</span>
+				</div>
+			</section>
+			<section>
+				<h2 class="detailsBackground">Details</h2>
+				<ul class="charDetails"></ul>
+				<div class="infoTabs"></div>
+				<ul class="charInfo"></ul>
+			</section>
+		</main>
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+// import Spinner from "../components/Spinner.vue";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+	components: {
+		// Spinner
+	},
+	data() {
+		return {
+			loading: true,
+			planetLoading: true,
+			currChar: null,
+		};
+	},
+	computed: {
+		chars() {
+			return this.$store.state.characters;
+		},
+		currPage() {
+			return this.$store.state.currPage;
+		},
+		maxPage() {
+			return this.$store.state.maxPage;
+		},
+	},
+	methods: {
+		async prevPage() {
+			await this.$store.dispatch("fetchPrevPage");
+		},
+		async nextPage() {
+			await this.$store.dispatch("fetchNextPage");
+		},
+	},
+	async mounted() {
+		await this.$store.dispatch("fetchChars");
+	},
+};
 </script>
+
+<style></style>
